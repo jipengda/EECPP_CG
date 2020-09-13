67bab8b06cb3a88f338a39b5312751d4f6d10780
Jipeng Dai <jipengda@buffalo.edu>
2020-09-12 21:32:21 -0400

    Update totalCost_calculation_by_set.py
    distance_lambda replace lamda
    turn_gamma replace garma

--- dual value/totalCost_calculation_by_set.py
+++ dual value/totalCost_calculation_by_set.py
@@ -30,14 +30,14 @@ def angle(second_to_lastNode, lastNode, newNode, coord_x, coord_y):
     return theta_degrees
 
 def totalCost_calculation_by_set(optimalSet, coord_x, coord_y, D):
-    lamda = 0.1164
-    garma = 0.0173
-    garma = 0 # so turning cost is 0
+    distance_lambda = 0.1164
+    turn_gamma = 0.0173
+    turn_gamma = 0.0172 # so turning cost is 0
     totalCost = 0
     lastNode=0
     newNode=optimalSet[1]
     turnCost=0
-    distanceCost= lamda * D[lastNode][newNode]
+    distanceCost= distance_lambda * D[lastNode][newNode]
     totalCost = turnCost + distanceCost
     length=len(optimalSet)
     for i in range(2, length):
@@ -46,7 +46,7 @@ def totalCost_calculation_by_set(optimalSet, coord_x, coord_y, D):
         second_to_lastNode = optimalSet[i-2]
         go = lastNode
         to = newNode
-        turnCost = garma * angle(second_to_lastNode, go, to, coord_x, coord_y)
-        distanceCost = lamda * D[go][to]
+        turnCost = turn_gamma * angle(second_to_lastNode, go, to, coord_x, coord_y)
+        distanceCost = distance_lambda * D[go][to]
         totalCost=totalCost+turnCost + distanceCost
     return totalCost
